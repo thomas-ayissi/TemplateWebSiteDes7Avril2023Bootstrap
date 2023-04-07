18a8f80dbca08e5b3ae6981925cb4fa8abfba653
Thomas Ayissi <thomas8sea@gmail.com>
2023-04-07 12:19:46 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//TechnoFutur-CoursJS-lun-29-aout2022-Exercice-Calculatrice
+
+// /*************CALCULATRICE**************/
+// 1.
+//   2 inputs pour les opérandes,
+//   1 select pour choisir l'opérateur
+//   et un bouton pour calculer le résultats.
+// 2. Afficher le résultat dans un h3
+// 3. Vérifier le cas de la division par 0
+
+
